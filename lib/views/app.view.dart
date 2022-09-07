a5987928aa575643a4c1dd0072e3d854173ecbcf
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-09-07 18:43:34 -0300

    Criando a casca do aplicativo com o MaterialApp

---
+++ lib/views/app.view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_search_book/theme/dark.theme.dart';
+import 'package:flutter_search_book/views/login.view.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: themeData,
+      routes: {
+        '/': (context) => const LoginView(),
+      },
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
