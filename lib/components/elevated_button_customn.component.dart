1026dc753574a858f362dd4a1a13affd0b57dbe4
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-09-07 18:46:31 -0300

    Criando componente reutilizável de botão

---
+++ lib/components/elevated_button_customn.component.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_search_book/theme/dark_class_theme.theme.dart';
+
+class ElevatedButtonCustomn extends StatelessWidget {
+  final String? textButton;
+
+  const ElevatedButtonCustomn({Key? key, required this.textButton})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 14.0),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          primary: DarkEnumColorsTheme.colorPurple.value,
+          fixedSize: Size(MediaQuery.of(context).size.width, 50.0),
+          textStyle: TextStyle(fontSize: DarkClassTheme().fontSize),
+          elevation: 10,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+        ),
+        child: Text('$textButton'),
+      ),
+    );
+  }
+}
