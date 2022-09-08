cf90b0d33c152a97ca6b6aaf92c8acf157e618ec
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-09-07 22:45:49 -0300

    Inicialiazando a aplicação Firebase antes de buildar o aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_search_book/views/app.view.dart';
 
-void main(List<String> args) {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const App());
 }
