0c771006b7b6cbd33935b8972b54b311f399144c
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-09-08 22:18:40 -0300

    Readequando o método login com o contexto da aplicação atual

--- lib/services/authentication.services.dart
+++ lib/services/authentication.services.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_search_book/services/navigation.services.dart';
 
 class AuthenticationException implements Exception {
   String message;
@@ -7,11 +11,14 @@ class AuthenticationException implements Exception {
 
 class AuthenticationService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final navigationService = NavigationService();
 
-  login(String email, String password) async {
+  login(BuildContext context, String email, String password) async {
     try {
       await _auth.signInWithEmailAndPassword(
-          email: email.toString(), password: password.toString());
+        email: email.toString(),
+        password: password.toString(),
+      );
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         throw AuthenticationException('Usuário não encontrado.');
@@ -20,5 +27,11 @@ class AuthenticationService {
         throw AuthenticationException('Senha incorreta. Tente novamente');
       }
     }
+
+    navigationService.redirectToHomeView(context);
+  }
+
+  userIsLogaded() {
+    return _auth.currentUser;
   }
 }
