622f714dd379e382888560f43d0f31ec24ba83db
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-09-08 22:17:14 -0300

    Removendo classe comentada

--- lib/components/alert_dialog_component.dart
+++ lib/components/alert_dialog_component.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_search_book/theme/dark_ligth_theme.theme.dart';
 
-// class AlertDialog extends StatelessWidget {
-//   const AlertDialog({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return showDialog(context: context, builder: () => AlertDialog());
-//   }
-// }
-
 AlertDialog showAlertDialog(BuildContext context, String message) =>
     AlertDialog(
       title: Text(
