be4c4957e8fd9cab6bf9074a18056e47eec6125c
Vitor-Kemuel <kemuvitor@gmail.com>
2022-09-23 15:55:46 -0300

    Criando novo textButton

---
+++ lib/components/text_button_custom.component.dart
@@ -0,0 +1,34 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class TextButtonCustom extends StatelessWidget {
+  const TextButtonCustom({
+    super.key,
+    this.child,
+    this.onPressed,
+    this.heigth,
+    this.width,
+  });
+  final Widget? child;
+  final VoidCallback? onPressed;
+  final Double? heigth;
+  final Double? width;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.all(15),
+        backgroundColor: Colors.blue,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(15.0),
+          ),
+        ),
+      ),
+      child: child ?? Container(),
+    );
+  }
+}
