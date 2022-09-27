b845405271f9c5d451d0ed40ecc2b4e572c09a5a
Vitor-Kemuel <kemuvitor@gmail.com>
2022-09-26 23:10:14 -0300

    implementado novo designer para a tela de login

--- lib/views/LoginView/login.view.dart
+++ lib/views/LoginView/login.view.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_search_book/components/alert_dialog.component.dart';
 import 'package:flutter_search_book/components/elevated_button_custom.component.dart';
 import 'package:flutter_search_book/components/text_form_input_customn.component.dart';
 import 'package:flutter_search_book/services/authentication.services.dart';
 import 'package:flutter_search_book/services/form.services.dart';
-import 'package:flutter_search_book/theme/dark.theme.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 
 class LoginView extends StatefulWidget {
@@ -22,92 +22,128 @@ class _LoginViewState extends State<LoginView> {
 
   final email = TextEditingController();
   final password = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     var formService = FormService(formKey: _formKey);
 
     return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: Center(
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.black,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(0),
+        child: AppBar(
+          elevation: 0,
+          systemOverlayStyle: const SystemUiOverlayStyle(
+            statusBarColor: Color.fromARGB(255, 33, 149, 243),
+            statusBarIconBrightness: Brightness.light,
+            statusBarBrightness: Brightness.light,
+          ),
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 33, 149, 243),
+              Color.fromARGB(255, 33, 149, 243),
+              Color.fromARGB(230, 33, 149, 243),
+              Color.fromARGB(200, 33, 149, 243),
+              Color.fromARGB(170, 33, 149, 243),
+              Color.fromARGB(150, 33, 149, 243),
+              Color.fromARGB(120, 33, 149, 243),
+              Color.fromARGB(100, 33, 149, 243),
+              Color.fromARGB(80, 33, 149, 243),
+              Color.fromARGB(60, 33, 149, 243),
+              Color.fromARGB(45, 33, 149, 243),
+              Color.fromARGB(40, 33, 149, 243),
+              Color.fromARGB(35, 33, 149, 243),
+              Color.fromARGB(30, 33, 149, 243),
+              Color.fromARGB(25, 33, 149, 243),
+              Color.fromARGB(20, 33, 149, 243),
+              Color.fromARGB(15, 33, 149, 243),
+              Color.fromARGB(10, 33, 149, 243),
+              Color.fromARGB(5, 33, 149, 243),
+              Color.fromARGB(0, 33, 149, 243),
+            ],
+          ),
+        ),
+        child: Form(
+          key: _formKey,
           child: ListView(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(20),
-                  ),
-                ),
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 5, 3),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(20),
-                    ),
-                  ),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color.fromARGB(122, 0, 0, 0),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: Offset(1, 0.5),
-                        ),
-                      ],
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.only(
-                        bottomRight: Radius.circular(20),
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height - 150,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        'lib/images/icon_logo.png',
+                        scale: 0.8,
                       ),
                     ),
-                    margin: EdgeInsets.fromLTRB(0, 0, 7, 3),
-                    child: Image.asset(
-                      'lib/images/icon_logo.png',
-                      scale: 0.6,
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(25, 0, 25, 0),
+                      child: TextFormInputCustomn(
+                        labelText: 'E-mail',
+                        prefixIcon: Icons.person_outlined,
+                        inputType: TextInputType.emailAddress,
+                        controller: email,
+                        validator: MultiValidator([
+                          RequiredValidator(errorText: 'E-mail é obrigatório'),
+                          EmailValidator(errorText: 'E-mail incorreto')
+                        ]),
+                      ),
                     ),
-                  ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(25, 0, 25, 0),
+                      child: TextFormInputCustomn(
+                        labelText: 'Senha',
+                        controller: password,
+                        prefixIcon: Icons.lock_outline,
+                        obscureText: true,
+                        isTextInputSecret: true,
+                        validator: MultiValidator([
+                          RequiredValidator(
+                            errorText: 'Senha é obrigatória',
+                          ),
+                          MinLengthValidator(
+                            6,
+                            errorText: 'Senha deve ter no minímo 6 caracteres',
+                          ),
+                        ]),
+                      ),
+                    ),
+                    (isLoading)
+                        ? const Padding(
+                            padding: EdgeInsets.only(top: 10.0),
+                            child: Center(
+                              child: CircularProgressIndicator(
+                                color: Colors.amber,
+                              ),
+                            ),
+                          )
+                        : Container(
+                            margin: const EdgeInsets.fromLTRB(40, 0, 40, 0),
+                            child: ElevatedButtonCustom(
+                              textButton: 'Login',
+                              onPressed: () {
+                                _login(formService, context);
+                              },
+                            ),
+                          ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                  ],
                 ),
               ),
-              TextFormInputCustomn(
-                labelText: 'E-mail',
-                prefixIcon: Icons.person_outlined,
-                controller: email,
-                validator: MultiValidator([
-                  RequiredValidator(errorText: 'E-mail é obrigatório'),
-                  EmailValidator(errorText: 'E-mail incorreto')
-                ]),
-              ),
-              TextFormInputCustomn(
-                labelText: 'Senha',
-                controller: password,
-                prefixIcon: Icons.lock_outline,
-                obscureText: true,
-                isTextInputSecret: true,
-                validator: MultiValidator([
-                  RequiredValidator(errorText: 'Senha é obrigatória'),
-                  MinLengthValidator(6,
-                      errorText: 'Senha deve ter no minímo 6 caracteres'),
-                ]),
-              ),
-              (isLoading)
-                  ? Padding(
-                      padding: const EdgeInsets.only(top: 10.0),
-                      child: Center(
-                        child: CircularProgressIndicator(
-                          color: colorPurple,
-                        ),
-                      ),
-                    )
-                  : ElevatedButtonCustom(
-                      textButton: 'Login',
-                      onPressed: () async {
-                        await _login(formService, context);
-                      },
-                    ),
             ],
           ),
         ),
@@ -116,8 +152,6 @@ class _LoginViewState extends State<LoginView> {
   }
 
   Future<void> _login(FormService formService, BuildContext context) async {
-    print(email.text);
-    print(password.text);
     setState(() => isLoading = true);
     if (formService.validateForm()) {
       formService.saveForm();
@@ -126,8 +160,9 @@ class _LoginViewState extends State<LoginView> {
       } on AuthenticationException catch (e) {
         setState(() => isLoading = false);
         showDialog(
-            context: context,
-            builder: (_) => showAlertDialog(context, e.message));
+          context: context,
+          builder: (_) => showAlertDialog(context, e.message),
+        );
       }
     }
 
