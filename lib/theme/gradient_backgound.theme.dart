fde227de25ee06c85fb13acccbd0bb2345bf4909
Vitor-Kemuel <kemuvitor@gmail.com>
2022-09-27 00:50:09 -0300

    Adicionando arquivo com as cores do background

---
+++ lib/theme/gradient_backgound.theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+List<Color> listColors = [
+  const Color.fromARGB(255, 33, 149, 243),
+  const Color.fromARGB(255, 33, 149, 243),
+  const Color.fromARGB(230, 33, 149, 243),
+  const Color.fromARGB(200, 33, 149, 243),
+  const Color.fromARGB(170, 33, 149, 243),
+  const Color.fromARGB(150, 33, 149, 243),
+  const Color.fromARGB(120, 33, 149, 243),
+  const Color.fromARGB(100, 33, 149, 243),
+  const Color.fromARGB(80, 33, 149, 243),
+  const Color.fromARGB(60, 33, 149, 243),
+  const Color.fromARGB(45, 33, 149, 243),
+  const Color.fromARGB(40, 33, 149, 243),
+  const Color.fromARGB(35, 33, 149, 243),
+  const Color.fromARGB(30, 33, 149, 243),
+  const Color.fromARGB(25, 33, 149, 243),
+  const Color.fromARGB(20, 33, 149, 243),
+  const Color.fromARGB(15, 33, 149, 243),
+  const Color.fromARGB(10, 33, 149, 243),
+  const Color.fromARGB(5, 33, 149, 243),
+  const Color.fromARGB(0, 33, 149, 243),
+];
